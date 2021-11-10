9036d4b2bffeeb43acdec7511ccc8de0b2c5acd6
Lev Erminson <erminson@gmail.com>
2021-11-10 12:57:49 +0300

    Add in config advisor bean

--- src/main/java/ru/erminson/lc/configuration/AppConfig.java
+++ src/main/java/ru/erminson/lc/configuration/AppConfig.java
@@ -1,6 +1,13 @@
 package ru.erminson.lc.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import ru.erminson.lc.repository.CourseRepository;
 import ru.erminson.lc.repository.RecordBookRepository;
@@ -9,6 +16,10 @@ import ru.erminson.lc.utils.CourseRepositoryYamlInitializer;
 import ru.erminson.lc.utils.RecordBookRepositoryInitializer;
 import ru.erminson.lc.utils.StudentRepositoryYamlInitializer;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
 @Configuration
 @ComponentScan("ru.erminson.lc")
 @EnableAspectJAutoProxy
@@ -28,5 +39,27 @@ public class AppConfig {
     @Autowired
     public RecordBookRepository createRecordBookRepository(StudentRepository studentRepository) {
         return RecordBookRepositoryInitializer.create(studentRepository);
+    }
+
+    @Value("${pointcut.property}")
+    private String pointcutExpression;
+
+    @Value("${pointcut.properties}")
+    private String[] pointcutExpressions;
+
+    @Bean
+    public Advisor advisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(pointcutExpression);
+        return new DefaultPointcutAdvisor(pointcut, new MethodInterceptor() {
+            @Override
+            public Object invoke(MethodInvocation invocation) throws Throwable {
+                List<?> args = Arrays.asList(invocation.getArguments());
+                log.info("Before:" + invocation.getMethod().getName() + ". Args: " + args);
+                Object returning = invocation.proceed();
+                log.info("After:" + invocation.getMethod().getName() + ". Return: " + returning);
+                return returning;
+            }
+        });
     }
 }
