96933e571d014a2e5f711a05ea2399a4a7ad6bf0
Lev Erminson <erminson@gmail.com>
2021-11-20 11:06:52 +0300

    Add schema.sql and data.sql

---
+++ learning-center/src/main/resources/sql/schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS student
+(
+    id         IDENTITY     NOT NULL PRIMARY KEY,
+    name       VARCHAR(128) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS topic
+(
+    id                IDENTITY     NOT NULL PRIMARY KEY,
+    title             VARCHAR(128) NOT NULL UNIQUE,
+    duration_in_hours SMALLINT     NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS course
+(
+    id    IDENTITY     NOT NULL PRIMARY KEY,
+    title VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS course_topic
+(
+    id        IDENTITY NOT NULL PRIMARY KEY,
+    course_id BIGINT REFERENCES course (id),
+    topic_id  BIGINT REFERENCES topic (id),
+    priority  SMALLINT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topic_score
+(
+    id       IDENTITY NOT NULL PRIMARY KEY,
+    topic_id BIGINT REFERENCES topic (id),
+    score    SMALLINT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS record_book
+(
+    id         IDENTITY NOT NULL PRIMARY KEY,
+    student_id BIGINT REFERENCES student (id),
+    course_id  BIGINT REFERENCES course (id),
+    start_date DATE     NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE IF NOT EXISTS record_book_topic_score
+(
+    id             IDENTITY NOT NULL PRIMARY KEY,
+    record_book_id BIGINT REFERENCES record_book (id),
+    topic_score_id BIGINT REFERENCES topic_score (id)
+);
