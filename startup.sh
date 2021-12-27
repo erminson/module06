0844677f77410b0f9760fa053f10ab3ad07307ae
Lev Erminson <erminson@gmail.com>
2021-12-27 09:38:35 +0300

    Create and config gateway service

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ./gradlew clean
 ./gradlew :discovery-service:bootJar
+./gradlew :gateway-service:bootJar
 ./gradlew :config-service:bootJar
 ./gradlew :learning-center:bootJar
 ./gradlew :payment-service:bootJar
